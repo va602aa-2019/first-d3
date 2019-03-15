701404b888d343000c2c6d9caa7f15a2f2151287
Salvo Rinzivillo <rinzivillo@isti.cnr.it>
2019-03-15 15:12:39 +0100

    Refactoring

--- src/main.js
+++ src/main.js
@@ -1,41 +1,38 @@
-const d3= require('d3');
+const d3 = require("d3");
 
-const numbers = [5,10,15];
+const numbers = [5, 10, 15];
+
+d3.select("#app")
+  .append("h1")
+  .text("My First d3.js application");
+
+const ul = d3.select("#app").append("ul");
+
+function redraw() {
+  let lis = ul.selectAll("li").data(numbers);
+
+  // exit
+  lis.exit().remove();
+
+  //enter
+  lis = lis
+    .enter()
+    .append("li")
+    .merge(lis);
+
+  //update
+  // lis.text('Element number is');
+  // lis.text(function(d,i){ return 'Element number is '+d  });
+  lis.text((d, i) => "Element number is " + d);
+}
+
+d3.select("#btnAdd").on("click", function() {
+  console.log("Add a number");
+});
+
+d3.select("#btnRemove").on("click", function() {
+  console.log("Remove a number");
+});
 
 
-d3.select('#app')
-  .append('h1')
-  .text('My First d3.js application');
-
-
-const ul = d3.select('#app')
-  .append('ul');
-
-
-let lis = ul.selectAll('li')
-  .data(numbers);
-
-// exit
-lis.exit().remove();
-
-//enter
-lis = lis.enter()
-  .append('li')
-  .merge(lis);
-
-//update
-// lis.text('Element number is');
-// lis.text(function(d,i){ return 'Element number is '+d  });
-lis.text((d,i) => ('Element number is '+d));
-
-
-d3.select('#btnAdd')
-  .on('click', function(){
-    console.log('Add a number');
-  });
-
-
-d3.select('#btnRemove')
-  .on('click', function(){
-    console.log('Remove a number');
-  });
+redraw();
